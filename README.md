2246995198817962df2fb4f3e6fe9963cbd0a637
Fab-i1 <fabienduc2016@gmail.com>
2025-07-26 19:02:31 +0200

    Fiin de semaine 1

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # Formation OCR & NLP - INRAE
 
-## Semaine 1 – OCR sur image (PNG)
+### Semaine 1
 
-Script `extract_text.py` permettant d'extraire du texte depuis une image d'aliment (type étiquette) à l'aide de Tesseract OCR.
+#### 25/07/2025 – OCR sur image (PNG)
 
-### Prérequis
+- Script `extract_text.py` permettant d'extraire du texte depuis une image à l'aide de Tesseract OCR.
+- **Prérequis :**
+  - Python 3.9+
+  - Tesseract installé localement
+  - Fichier `fra.traineddata` à placer dans `semaine_01_ocr_pdf/tessdata/`
+- **Installation des dépendances :**
+  ```bash
+  pip install pillow pytesseract
+  ```
 
-- Python 3.9+
+#### 26/07/2025 – OCR depuis un PDF
+
+- Extraction de texte depuis des fichiers PDF (via `extract_text_from_pdf.py` + `pdf2image`)
 - Tesseract installé localement
 - Fichier `fra.traineddata` à placer dans `semaine_01_ocr_pdf/tessdata/`
-
-### Installation des dépendances
-
-```bash
-pip install pillow pytesseract
-```
-
-nouvelle essai
--- semaine_01_ocr_pdf/extract_text_from_pdf.py
+++ semaine_01_ocr_pdf/extract_text_from_pdf.py
@@ -1,10 +1,22 @@
 from pdf2image import convert_from_path
-from  pytesseract import pytesseract as pyt
+import pytesseract
 
 import os
 
-# Modifier le chemin vers ton exécutable Tesseract si nécessaire
-pyt.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# # Modifier le chemin vers ton exécutable Tesseract si nécessaire
+# pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Chemin Tesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+# Chemin vers le dossier local tessdata
+tessdata_path = os.path.join(os.path.dirname(__file__), "tessdata")
+print("TESSDATA path:", tessdata_path)
+print("Contenu du dossier tessdata:", os.listdir(tessdata_path))
+
+os.environ["TESSDATA_PREFIX"] = tessdata_path
+
 
 # Dossier contenant le fichier PDF
 pdf_path = os.path.join(os.path.dirname(__file__),"modele_de_facture.pdf")
@@ -14,8 +26,9 @@ print ("Chemin du PDF:", pdf_path)
 images = convert_from_path(pdf_path)
 print (f"Nombre de pages converties: {len(images)}")
 
-
-# # Parcourir les pages et extraire le texte
-# for i, image in enumerate(images):
-#     text = pyt.image_to_string(image, lang="fra")
-#     print(f"\n--- Texte extrait de la page {i+1} ---\n{text}")
+with open("extracted_text_from_pdf.txt", "w", encoding="utf-8") as f:
+    # Parcourir les pages et extraire le texte
+    for i, image in enumerate(images):
+        text = pytesseract.image_to_string(image, lang="fra")
+        print(f"\n--- Texte extrait de la page {i+1} ---\n{text}")
+        f.write(f"\n--- Texte extrait de la page {i+1} ---\n{text}")
